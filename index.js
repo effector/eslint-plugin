42c1f47630d7ef42b2ecaf34d3f151698a0a7888
Igor Kamyshev <igor@kamyshev.me>
2021-08-28 23:13:21 +0700

    Hotfix: add no-getState to public API

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = {
   rules: {
     "enforce-store-naming-convention": require("./rules/enforce-store-naming-convention/enforce-store-naming-convention"),
     "enforce-effect-naming-convention": require("./rules/enforce-effect-naming-convention/enforce-effect-naming-convention"),
+    "no-getState": require("./rules/no-getState/no-getState"),
   },
   configs: {
     recommended: require("./config/recommended"),
