4ac253051d597171bee41982130b2a23c83e9f00
Igor Kamyshev <igor@kamyshev.me>
2021-08-28 18:20:01 +0700

    Fix rule nameing strategy

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
   rules: {
-    "effector/enforce-store-naming-convention": require("./rules/enforce-store-naming-convention/enforce-store-naming-convention"),
+    "enforce-store-naming-convention": require("./rules/enforce-store-naming-convention/enforce-store-naming-convention"),
   },
   configs: {
     recommended: require("./config/recommended"),
