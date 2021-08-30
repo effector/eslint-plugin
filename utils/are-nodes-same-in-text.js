a85779f8ba809a6ef74e16c7e471a9822ded8a74
Igor Kamyshev <igor@kamyshev.me>
2021-08-30 22:33:24 +0700

    Add wrapping for prettier invalid statement

--- utils/are-nodes-same-in-text.js
+++ utils/are-nodes-same-in-text.js
@@ -1,11 +1,20 @@
 const prettier = require("prettier");
 
 function areNodesSameInText({ context, nodes }) {
-  const texts = nodes.map((node) =>
-    prettier.format(context.getSourceCode().getText(node), {
+  const texts = nodes.map((node) => {
+    let sourceText = context.getSourceCode().getText(node);
+
+    const shouldBeWrapped =
+      sourceText.startsWith("{") && sourceText.endsWith("}");
+
+    if (shouldBeWrapped) {
+      sourceText = `(${sourceText})`;
+    }
+
+    return prettier.format(sourceText, {
       parser: "babel-ts",
-    })
-  );
+    });
+  });
 
   return texts.every((text) => text === texts[0]);
 }
