7d79b1177e0ab108e9ce8d1582cd340abeaa24b0
Igor Kamyshev <igor@kamyshev.me>
2021-09-23 23:57:35 +0700

    Add basic implementation of no-unnecessary-combination

--- rules/no-unnecessary-combination/no-unnecessary-combination.js
+++ rules/no-unnecessary-combination/no-unnecessary-combination.js
@@ -24,7 +24,61 @@ module.exports = {
       ImportDeclaration(node) {
         extractImportedFromEffector(importedFromEffector, node);
       },
-      CallExpression(node) {},
+      CallExpression(node) {
+        const METHODS_WITH_POSSIBLE_UNNECESSARY_COMBINATION = [
+          "sample",
+          "guard",
+          "forward",
+        ];
+
+        const CONFIG_ARG_PROPERTIES = ["source", "clock", "from"];
+
+        const UNNECESSARY_METHODS = ["combine", "merge"];
+        const localUnnecessaryMethods = UNNECESSARY_METHODS.map((m) =>
+          importedFromEffector.get(m)
+        ).filter(Boolean);
+
+        for (const method of METHODS_WITH_POSSIBLE_UNNECESSARY_COMBINATION) {
+          const localMethod = importedFromEffector.get(method);
+          if (!localMethod) {
+            continue;
+          }
+
+          const isEffectorMethod = node?.callee?.name === localMethod;
+          if (!isEffectorMethod) {
+            continue;
+          }
+
+          const candidates =
+            node?.arguments?.[0]?.properties?.filter((n) =>
+              CONFIG_ARG_PROPERTIES.includes(n.key.name)
+            ) ?? [];
+
+          if (candidates.length === 0) {
+            continue;
+          }
+
+          for (const candidate of candidates) {
+            const candidateName = candidate?.value?.callee?.name;
+            if (!candidateName) {
+              continue;
+            }
+
+            const UnnecessaryMethodIsEffectorMethod =
+              localUnnecessaryMethods.some((m) => m === candidateName);
+
+            if (!UnnecessaryMethodIsEffectorMethod) {
+              continue;
+            }
+
+            context.report({
+              node: candidate?.value,
+              messageId: "unnecessaryCombination",
+              data: { methodName: candidateName },
+            });
+          }
+        }
+      },
     };
   },
 };
