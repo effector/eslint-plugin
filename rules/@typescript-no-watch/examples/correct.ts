22a008cf8dca111f5825e018c45d81fc0918ca8c
Viktor Pasynok <v.pasynok@space307.com>
2021-10-02 17:39:20 +0300

    test: add more correct examples for no-watch rule

--- rules/@typescript-no-watch/examples/correct.ts
+++ rules/@typescript-no-watch/examples/correct.ts
@@ -3,5 +3,66 @@ const event = {
     return {};
   },
 };
+const effect = {
+  done: {
+    watch() {
+      return {};
+    },
+  },
+  fail: {
+    watch() {
+      return {};
+    },
+  },
+  doneData: {
+    watch() {
+      return {};
+    },
+  },
+  failData: {
+    watch() {
+      return {};
+    },
+  },
+  finally: {
+    watch() {
+      return {};
+    },
+  },
+  watch() {
+    return {};
+  },
+};
+const $ = {
+  updates: {
+    watch() {
+      return {};
+    },
+  },
+  watch() {
+    return {};
+  },
+};
+const sample = {
+  watch() {
+    return {};
+  },
+};
+const guard = {
+  watch() {
+    return {};
+  },
+};
 
-export const value = event.watch();
+event.watch();
+effect.done.watch();
+effect.fail.watch();
+effect.doneData.watch();
+effect.failData.watch();
+effect.finally.watch();
+$.watch();
+$.updates.watch();
+sample.watch();
+guard.watch();
+
+export const T = () => true;
