29c45fa1cb18947213ca8ba3cbe0d641c72033c6
Igor Kamyshev <igor@kamyshev.me>
2022-04-05 17:35:48 +0700

    Add test for exported rules

--- puglin.test.js
+++ puglin.test.js
@@ -1,3 +1,6 @@
+const { readdir } = require("fs/promises");
+const { join } = require("path");
+
 const plugin = require("./index");
 const { createLinkToRule } = require("./utils/create-link-to-rule");
 
@@ -8,5 +11,19 @@ describe("plugin", () => {
     Object.entries(rules).forEach(([name, rule]) => {
       expect(rule.meta.docs.url).toBe(createLinkToRule(name));
     });
+  });
+
+  test("any rule should be exported", async () => {
+    const rulesDirContent = await readdir(join(__dirname, "rules"));
+
+    const allRules = rulesDirContent
+      .filter((fileName) => !fileName.includes(".json"))
+      .sort();
+
+    const exportedRules = Object.entries(plugin.rules)
+      .map(([name]) => name)
+      .sort();
+
+    expect(exportedRules).toEqual(allRules);
   });
 });
