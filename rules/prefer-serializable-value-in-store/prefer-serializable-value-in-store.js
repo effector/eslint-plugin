3c746835c7994a9e4f28282c8b6985fcaa917f13
Igor Kamyshev <igor@kamyshev.me>
2022-05-02 13:58:08 +0700

    Add naive implmentation

--- rules/prefer-serializable-value-in-store/prefer-serializable-value-in-store.js
+++ rules/prefer-serializable-value-in-store/prefer-serializable-value-in-store.js
@@ -2,6 +2,7 @@ const { createLinkToRule } = require("../../utils/create-link-to-rule");
 const { extractImportedFrom } = require("../../utils/extract-imported-from");
 const { is } = require("../../utils/is");
 const { extractType } = require("../../utils/extract-type");
+const { walkType } = require("../../utils/walk-type");
 
 module.exports = {
   meta: {
@@ -33,11 +34,75 @@ module.exports = {
           return;
         }
 
+        const useSerialization = isStoreSerializeble({ node });
+
         const storeType = extractType({ node, context });
         const valueType = storeType?.resolvedTypeArguments?.[0];
 
-        // TODO: analyze valueType
+        let dangerousType = null;
+        walkType(
+          valueType,
+          (typeItem) => {
+            if (isPrimitive(typeItem)) {
+              return;
+            }
+
+            if (isDate(typeItem)) {
+              dangerousType = typeItem;
+              return;
+            }
+          },
+          { context }
+        );
+
+        if (dangerousType && useSerialization) {
+          context.report({
+            node,
+            messageId: "dangerousType",
+            data: {
+              typeName: typeName(dangerousType) ?? "unknown",
+            },
+          });
+        }
       },
     };
   },
 };
+
+// TypeCheckers
+
+function typeName(type) {
+  return type?.symbol?.escapedName ?? type?.typeName?.escapedText;
+}
+
+function isPrimitive(type) {
+  return ["boolean", "number", "string"].includes(type?.intrinsicName);
+}
+
+function isDate(type) {
+  return typeName(type) === "Date" ?? false;
+}
+
+// Arg extraction
+function isStoreSerializeble({ node }) {
+  if (node?.init?.type !== "CallExpression") {
+    return true;
+  }
+
+  if (node?.init?.callee?.name === "combine") {
+    return false;
+  }
+
+  if (node?.init?.callee?.property?.name === "map") {
+    return false;
+  }
+
+  if (node?.init?.callee?.name === "createStore") {
+    const serializeProperty = node?.init?.arguments?.[1]?.properties?.find(
+      (prop) => prop?.key?.name === "serialize"
+    );
+    return serializeProperty?.value?.value !== "ignore" ?? true;
+  }
+
+  return true;
+}
