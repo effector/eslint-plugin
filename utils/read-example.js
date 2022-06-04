cbc6a21f588f07c2d1dcfc8efe8b9b46544edf93
Alexander Khoroshikh <32790736+AlexandrHoroshih@users.noreply.github.com>
2022-06-04 14:43:44 +0700

    Add util to read all examples

--- utils/read-example.js
+++ utils/read-example.js
@@ -1,8 +1,45 @@
 const { readFileSync } = require("fs");
 const { join } = require("path");
+const glob = require("glob");
 
 function readExample(dirname, exampleName) {
   return readFileSync(join(dirname, "examples", exampleName)).toString();
 }
 
-module.exports = { readExample };
+function getCorrectExamples(dirname, config = {}) {
+  const { ext = "js", namesOnly = true } = config;
+  const pattern = `correct-*.${ext}`;
+  const correct = glob.sync(join(dirname, "examples", pattern));
+
+  let result = correct;
+
+  if (namesOnly) {
+    result = result.map((path) => {
+      const rightSlashIdx = path.lastIndexOf("/");
+
+      return path.slice(rightSlashIdx + 1);
+    });
+  }
+
+  return result;
+}
+
+function getIncorrectExamples(dirname, config = {}) {
+  const { ext = "js", namesOnly = true } = config;
+  const pattern = `incorrect-*.${ext}`;
+  const incorrect = glob.sync(join(dirname, "examples", pattern));
+
+  let result = incorrect;
+
+  if (namesOnly) {
+    result = result.map((path) => {
+      const rightSlashIdx = path.lastIndexOf("/");
+
+      return path.slice(rightSlashIdx + 1);
+    });
+  }
+
+  return result;
+}
+
+module.exports = { readExample, getCorrectExamples, getIncorrectExamples };
