427e5dfcb617504c966e323948c5a4b87c70c2f2
ivan posokhin <i.posokhin@redmadrobot.com>
2022-09-03 18:32:36 +0500

    Split tests of rule enforce-store-naming-convention to prefix and postfix cases

--- rules/enforce-store-naming-convention/examples/incorrect-store-naming-in-domain.js
+++
@@ -1,7 +0,0 @@
-import { createDomain } from "effector";
-
-const d = createDomain();
-
-const sum = d.createStore(0);
-
-export { sum };
--- rules/enforce-store-naming-convention/examples/incorrect-store-naming.js
+++
@@ -1,5 +0,0 @@
-import { createStore } from "effector";
-
-const sum = createStore(0);
-
-export { sum };
