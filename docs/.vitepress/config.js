66e99079c3ee652f810a22e99a5092cf5244c4f8
Igor Kamyshev <igor@kamyshev.me>
2022-11-14 16:58:28 +0700

    Setup vitepress for future docs site

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,33 @@
+export default {
+  lang: "en-US",
+  title: "ESLint plugin for Effector",
+  lastUpdated: true,
+  head: [
+    // ['link', { rel: 'icon', href: '/favicon.ico', sizes: 'any' }],
+    // ['link', { rel: 'icon', href: '/icon.svg', type: 'image/svg+xml' }],
+    // ['link', { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' }],
+    // ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
+  ],
+  themeConfig: {
+    siteTitle: "ESLint plugin for Effector",
+    // logo: '/logo.svg',
+    footer: {
+      message: "Released under the MIT License.",
+    },
+    socialLinks: [
+      { icon: "github", link: "https://github.com/effector/eslint-plugin" },
+      { icon: "twitter", link: "https://twitter.com/effectorjs" },
+    ],
+    // algolia: {
+    //   appId: 'kkk',
+    //   apiKey: 'lll',
+    //   indexName: 'eslint-plugin-effector',
+    // },
+    editLink: {
+      pattern:
+        "https://github.com/effector/eslint-plugin/edit/master/docs/:path",
+    },
+    nav: [],
+    sidebar: {},
+  },
+};
---
+++ docs/index.md
@@ -0,0 +1,18 @@
+---
+layout: home
+
+hero:
+  name: ESLint plugin
+  text: Effector's family
+  tagline: Enforcing best practices for Effector
+  #   image:
+  #     src: /logo.svg
+  #     alt: ESLint plugin for Effector
+  actions:
+    - theme: brand
+      text: Presents
+      link: /presets/
+    - theme: alt
+      text: Rules
+      link: /rules
+---
