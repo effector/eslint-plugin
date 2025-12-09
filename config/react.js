d687cb36f45d09f9543b081b3aa0fc89fa5a09f0
Ilya Olovyannikov <excaliburgyro@gmail.com>
2025-12-09 12:56:00 +0300

    feat(rule): add new rule for destructured units

--- config/react.js
+++ config/react.js
@@ -3,5 +3,6 @@ module.exports = {
     "effector/enforce-gate-naming-convention": "error",
     "effector/mandatory-scope-binding": "error",
     "effector/prefer-useUnit": "warn",
+    "effector/use-unit-destructuring": "warn",
   },
 };
--- index.js
+++ index.js
@@ -20,6 +20,7 @@ module.exports = {
     "prefer-useUnit": require("./rules/prefer-useUnit/prefer-useUnit"),
     "require-pickup-in-persist": require("./rules/require-pickup-in-persist/require-pickup-in-persist"),
     "no-patronum-debug": require("./rules/no-patronum-debug/no-patronum-debug"),
+    "use-unit-destructuring": require("./rules/use-unit-destructuring/use-unit-destructuring"),
   },
   configs: {
     recommended: require("./config/recommended"),
